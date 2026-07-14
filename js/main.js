6fd700599bf83b821828142f3285e8ad9662f726
sandhyarani-04232022 <sandhyaranijangam@gmail.com>
2026-07-14 13:37:09 +0530

    Portfolio updated with new changes

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
